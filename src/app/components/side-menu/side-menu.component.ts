8eab6e9f0c5118fcfc6c1497eb15e3693e222067
khachatryanyuri <khachatryanyuri94@mail.ru>
2024-08-14 15:53:19 +0400

    fix links.

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { MenuItem } from '../../types';
 import { sections } from '../../constants';
@@ -12,13 +12,13 @@ import { sections } from '../../constants';
   standalone: true,
   imports: [MatListModule, MatButtonModule, MatIconModule],
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss'],
+  styleUrl: './side-menu.component.scss',
 })
 export class SideMenuComponent implements OnInit {
   sections: MenuItem[] = sections;
   activeSection: string = sections[0].route;
 
-  constructor(private _route: ActivatedRoute) {}
+  constructor(private _route: ActivatedRoute, private _router: Router) {}
 
   onItemClicked(item: MenuItem) {
     this.scrollToSection(item.route);
@@ -27,8 +27,11 @@ export class SideMenuComponent implements OnInit {
   scrollToSection(route: string) {
     const element = document.getElementById(route);
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
       this.activeSection = route;
+      element.scrollIntoView({ behavior: 'smooth' });
+      this._router.navigate([], {
+        fragment: route,
+      });
     }
   }
 
